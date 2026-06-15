0e08feef6538085dc4cf5dae61dfef20dcd862f1
soyul <soyul9280@gmail.com>
2026-06-15 16:10:06 +0900

    chore: 초기 프로젝트 세팅 작업

---
+++ src/main/java/project/orderminisystem/OrderMiniSystemApplication.java
@@ -0,0 +1,13 @@
+package project.orderminisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMiniSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMiniSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/orderminisystem/OrderMiniSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project.orderminisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMiniSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
